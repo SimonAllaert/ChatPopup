87e43d49b8be8d8fff56d4937aec8a46f4d9877b
Simon Allaert <simon.allaert@mendify.eu>
2025-05-06 16:16:07 +0200

    quick tinkering

--- js/chat_socket.js
+++ js/chat_socket.js
@@ -8,7 +8,7 @@ socket.addEventListener('message', function (event) {
 });
 
 
-const chat1 = {id: 42, user: "mrscooby_doo", color: "#1E90FF", content: "MONKEEEY!!!"};
+const chat1 = {id: 42, user: "mrscooby_doo", color: "#1E90FF", content: "MONK KEKW EEEY!!!"};
 const chat2 = {id: 44, user: "stellamelonn", color: "#FF69B4", content :"im gonna touch you"};
 const chat3 = {id: 50, user: "dubstep_dalton", color: "#C91F1F", content: "ant to submit a video? Rules: Vid KEKW must be safe for Twitch, 60 seconds or less, and be a working YouTube, Tiktok, Instagram, or Twitter link. Each request costs $3. Click this link and put your vid link in the donation message by itself: https://streamelements.com/jimmyhere/tip All tips are final and much appreciated. I have the right to not watch/listen to requests."}
 
@@ -33,6 +33,7 @@ const loadChatMessage = (message_object) => {
 
     const content = document.createElement("span");
     content.classList.add("message-content");
+    content.innerText = ': ';
     let message_content = message_object.content.split(" ");
     for (token of message_content) {
         const emote = emotes.get(token);
@@ -52,7 +53,7 @@ const loadChatMessage = (message_object) => {
             content.append(`${token} `);
         }
     }
-    //content.innerText = `: ${message_object.content}`;
+    //content.innerText = `: ${content.innerText}`;
     chat_message.appendChild(content);
 
     if (scrolled_to_bottom) {
@@ -63,4 +64,9 @@ const loadChatMessage = (message_object) => {
 const emotes = new Map();
 emotes.set("KEKW", "https://cdn.frankerfacez.com/emoticon/381875/1");
 
-loadChatMessage(chat3);
+loadChatMessage(chat2);
+loadChatMessage(chat2);
+loadChatMessage(chat1);
+loadChatMessage(chat2);
+loadChatMessage(chat2);
+loadChatMessage(chat2);
